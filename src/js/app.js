ac8a5168ec8504668e04061cc40e145e5b13cde9
vorpalvunny <vorpal.vunny.poker.taipei@gmail.com>
2022-07-16 23:30:13 +0800

    fixed: remove version query for images

--- src/js/app.js
+++ src/js/app.js
@@ -10,7 +10,7 @@ export class App {
    * @param {string} [VERSION='4.0.0']
    * @memberof App
    */
-  constructor(VERSION = '5.0.0') {
+  constructor(VERSION = '5.0.1') {
     this.config = {
       version: VERSION,
       storeKey: 'VB_PAGE',
