fc390b529d1fcb87519411ddfdba54c19fc5141f
vorpalvunny <vorpal.vunny.poker.taipei@gmail.com>
2022-07-18 06:54:12 +0800

    refactor

--- src/js/app.js
+++ src/js/app.js
@@ -10,7 +10,7 @@ export class App {
    * @param {string} [VERSION='4.0.0']
    * @memberof App
    */
-  constructor(VERSION = '5.0.23') {
+  constructor(VERSION = '5.0.24') {
     this.config = {
       version: VERSION,
       storeKey: 'VB_PAGE',
